d04bf0742cbfaee00098d424b5dbd83697c45291
mizzyjo <86835729+mizzyjo@users.noreply.github.com>
2022-04-10 19:19:44 +0900

    JS: Active navbar

--- main.js
+++ main.js
@@ -1,77 +1,77 @@
-"use strict";
+'use strict';
 
 // Make navbar transparent when it is on the top
-const navbar = document.querySelector("#navbar");
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   // console.log(window.scrollY);
   // console.log(navbarHeight);
   if (window.scrollY > navbarHeight) {
-    navbar.classList.add("navbar--dark");
+    navbar.classList.add('navbar--dark');
   } else {
-    navbar.classList.remove("navbar--dark");
+    navbar.classList.remove('navbar--dark');
   }
 });
 
 // Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector(".navbar__menu");
+const navbarMenu = document.querySelector('.navbar__menu');
 
-navbarMenu.addEventListener("click", (event) => {
+navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
-  navbarMenu.classList.remove("open");
+  navbarMenu.classList.remove('open');
   console.log(event.target.dataset.link);
   scrollIntoView(link);
   // selectNavItem(target);
 });
 
 // Navbar toggle button for small screen
-const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
-navbarToggleBtn.addEventListener("click", () => {
-  navbarMenu.classList.toggle("open");
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "contact me" button on home
-const homeContactBtn = document.querySelector(".home__contact");
+const homeContactBtn = document.querySelector('.home__contact');
 
-homeContactBtn.addEventListener("click", (event) => {
-  scrollIntoView("#contact");
+homeContactBtn.addEventListener('click', (event) => {
+  scrollIntoView('#contact');
 });
 
 // Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector(".home__container");
+const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 // Show "arrow up" button when scrolling down
-const arrowUp = document.querySelector(".arrow-up");
-document.addEventListener("scroll", () => {
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
   if (window.scrollY > homeHeight / 2) {
-    arrowUp.classList.add("visible");
+    arrowUp.classList.add('visible');
   } else {
-    arrowUp.classList.remove("visible");
+    arrowUp.classList.remove('visible');
   }
 });
 
 // Handle click on the "arrow up" button
-arrowUp.addEventListener("click", () => {
-  scrollIntoView("#home");
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
 });
 
 // Projects
-const workBtnContainer = document.querySelector(".work__categories");
-const projectContainer = document.querySelector(".work__projects");
-const projects = document.querySelectorAll(".project");
-const categoryBtns = document.querySelectorAll(".category__btn");
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+const categoryBtns = document.querySelectorAll('.category__btn');
 
-workBtnContainer.addEventListener("click", (e) => {
+workBtnContainer.addEventListener('click', (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   console.log(filter);
   if (filter == null) {
@@ -79,23 +79,23 @@ workBtnContainer.addEventListener("click", (e) => {
   }
 
   // Remove selection from the previous item and select the new one
-  const active = document.querySelector(".category__btn.selected");
-  active.classList.remove("selected");
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
   const target =
-    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
-  target.classList.add("selected");
+    e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  target.classList.add('selected');
 
-  projectContainer.classList.add("anim-out");
+  projectContainer.classList.add('anim-out');
 
   setTimeout(() => {
     projects.forEach((project) => {
-      if (filter === "*" || filter === project.dataset.type) {
-        project.classList.remove("invisible");
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
       } else {
-        project.classList.add("invisible");
+        project.classList.add('invisible');
       }
     });
-    projectContainer.classList.remove("anim-out");
+    projectContainer.classList.remove('anim-out');
   }, 300);
 });
 
@@ -111,12 +111,12 @@ workBtnContainer.addEventListener("click", (e) => {
 // 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
 
 const sectionIds = [
-  "#home",
-  "#about",
-  "#skills",
-  "#work",
-  "#testimonials",
-  "#contact",
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonials',
+  '#contact',
 ];
 
 const sections = sectionIds.map((id) => document.querySelector(id));
@@ -130,52 +130,13 @@ let selectedNavIndex = 0;
 
 let selectedNavItem = navItems[0];
 function selectNavItem(selected) {
-  selectedNavItem.classList.remove("active");
+  selectedNavItem.classList.remove('active');
   selectedNavItem = selected;
-  selectedNavItem.classList.add("active");
+  selectedNavItem.classList.add('active');
 }
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: "smooth", block: "center" });
+  scrollTo.scrollIntoView({ behavior: 'smooth', block: 'center' });
   selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
-
-const observerOptions = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.3,
-};
-
-const observerCallback = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
-      const index = sectionIds.indexOf(`#${entry.target.id}`);
-      // let selectedIndex;
-      // 스크롤링이 아래로 되어서 페이지가 올라옴
-      if (entry.boundingClientRect.y < 0) {
-        selectedNavIndex = index + 1;
-      } else {
-        selectedNavIndex = index - 1;
-      }
-      // selectedNavItem.classList.remove("active");
-      // selectedNavItem = navItems[selectedIndex];
-      // selectedNavItem.classList.add("active");
-    }
-  });
-};
-
-const observer = new IntersectionObserver(observerCallback, observerOptions);
-sections.forEach((section) => observer.observe(section));
-
-window.addEventListener("wheel", () => {
-  if (window.scrollY === 0) {
-    selectedNavIndex = 0;
-  } else if (
-    window.scrollY + window.innerHeight ===
-    document.body.clientHeight
-  ) {
-    selectedNavIndex = navItems.length - 1;
-  }
-  selectNavItem(navItems[selectedNavIndex]);
-});
